89a85ee85619901a76df226d425c97c3a808180e
Mohamed AIT MANSOUR <maitmansour@clever-age.com>
2020-05-03 19:29:50 +0200

    Ajout des commentaires - Auth et Gameplay

--- src/controllers/Auth.py
+++ src/controllers/Auth.py
@@ -2,6 +2,8 @@ from src.models.Joueur import Joueur
 from src import db,engine
 import cx_Oracle
 class Auth:
+
+    # fonction de connexion utilisateur, retourne None si la connexion est échouée
     @staticmethod
     def attempt(username,password):
         j = Joueur.query.filter_by(identifiant=username).first()
@@ -10,6 +12,7 @@ class Auth:
         else :
             return None
 
+    # fonction de creation d'un compte utilisateur, elle appelle la procédure enregistrer_joueur
     @staticmethod
     def create(username,password):
         connection = engine.raw_connection()
--- src/controllers/Gameplay.py
+++ src/controllers/Gameplay.py
@@ -3,14 +3,18 @@ from src.models.Coup import Coup
 from src import db,engine
 import cx_Oracle
 class Gameplay:
+
+    # la fonction de creation d'une nouvelle partie, elle appelle la fonction create_partie du Model Partie
     @staticmethod
     def newgame(user_id,level_id):
         return Partie.create_partie(user_id,level_id)
         
+    # la fonction qui maruqe la fin d'une partie, elle appelle la fonction end_partie du Model Partie
     @staticmethod
     def endgame(user_id,idpartie,etat,score):
         return Partie.end_partie(user_id,idpartie,etat,score)
 
+    # Cette fonction permet l'enregistrement d'un coup, elle appelle la procédure enregistrer_coup de la bdd
     @staticmethod
     def savecoup(idbille,idpartie,depart,arrivee):
         connection = engine.raw_connection()
@@ -20,6 +24,7 @@ class Gameplay:
         connection.close()
         return True
 
+    # cette fonction retourne toutes la parties à partir d'un Id Utilisateur
     @staticmethod
     def mine(id_user):
         parties = Partie.query.filter_by(id_user=id_user,etat=1).order_by("idpartie").all()
@@ -35,6 +40,7 @@ class Gameplay:
             })
         return data
 
+    # Cette fonction retourne tout les coups dune partie déjà jouer afin de simuler une partie
     @staticmethod
     def revoire(idpartie):
         coups = Coup.query.filter_by(idpartie=idpartie).order_by("idcoup").all()
